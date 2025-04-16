b88e4e976d747358b7380064f4e6461a839911fe
Milton Figueredo <gratno@gmail.com>
2025-04-16 15:49:16 -0500

    organizado add_movi request_json

--- src/routes/Movie.py
+++ src/routes/Movie.py
@@ -1,5 +1,6 @@
 from flask import Blueprint 
 from flask import jsonify
+from flask import request
 
 #models
 from models.MovieModel import MovieModel
@@ -31,15 +32,15 @@ def get_movie(id):
     except Exception as ex:
         return jsonify({'message':str(ex)}), 500
     
-@main.route('/<id>')
-def get_movie(id):
+@main.route('/add',methods=['POST'])
+def add_movie():
     try:
         
-        movie=MovieModel.get_movie(id)
-        if movie!=None:
-            return jsonify(movie)
-        else:
-            return jsonify({}),404
+        title=request.json['title']
+        duration=int(request.json['duration'])
+        released=request.json['released']
+        
+        return jsonify({})
         
     except Exception as ex:
         return jsonify({'message':str(ex)}), 500
